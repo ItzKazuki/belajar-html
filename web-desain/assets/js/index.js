9b53ad8c309d952ae358fdf5467553ecc3732602
ItzKazuki <ibnu235729@gmail.com>
2023-10-15 19:38:27 +0700

    fix card

--- web-desain/assets/js/index.js
+++ web-desain/assets/js/index.js
@@ -2,7 +2,27 @@ window.onscroll = () => {
   var navbar = document.getElementById("navbar");
   var sticky = navbar.offsetTop;
 
-  console.log(sticky)
-
   window.scrollY >= sticky ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
+
+// const swiper = new Swiper('.cards', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // // If we need pagination
+//   // pagination: {
+//   //   el: '.swiper-pagination',
+//   // },
+
+//   // // Navigation arrows
+//   // navigation: {
+//   //   nextEl: '.swiper-button-next',
+//   //   prevEl: '.swiper-button-prev',
+//   // },
+
+//   // // And if we need scrollbar
+//   // scrollbar: {
+//   //   el: '.swiper-scrollbar',
+//   // },
+// });
